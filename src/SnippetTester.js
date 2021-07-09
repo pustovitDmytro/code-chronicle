9ba74ac9232fe24e2c3567e3ed6f6b72af62214f
Dmytro Pustovit <dmytro.pustovit@gmail.com>
2021-07-10 02:29:40 +0300

    New: allow errors in snippet tests

--- src/SnippetTester.js
+++ src/SnippetTester.js
@@ -8,10 +8,19 @@ export default class SnippetTester {
     }
 
     test = async (func, expected) => {
-        const result = await func(...this.args);
+        try {
+            const result = await func(...this.args);
 
-        if (expected) this.tester(result, expected);
+            if (expected) this.tester(result, expected);
 
-        if (this.mocha) await this.mocha.saveSnippetTest(result);
+            if (this.mocha) await this.mocha.saveSnippetTest(result);
+        } catch (error) {
+            if (error.name !== 'AssertionError' && expected && expected instanceof Error) {
+                assert.equal(error.message, expected.message);
+            // TODO: save examples
+            } else {
+                throw error;
+            }
+        }
     }
 }
